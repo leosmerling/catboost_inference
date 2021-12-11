4c43897769f951216e2a287a723e96381aa9a055
Leo <leosmerling@noreply.github.com>
2021-12-11 22:12:38 +0100

    Batch prediction

--- rust/grpc-server/src/client.rs
+++ rust/grpc-server/src/client.rs
@@ -10,40 +10,58 @@ const REPORT: usize = 1000;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("Usage: cb-client http://host:port iterations timeout_ms");
-
+    println!("========================================================================================================================================================");
+    println!("Usage: cb-client http://host:port batch_size iterations timeout_ms");
     let host = std::env::args().nth(1).expect("http://host:port missing");
-    let n: usize = std::env::args().nth(2).expect("missing number iterations").parse().unwrap();
-    let timeout: u64 = std::env::args().nth(3).expect("missing timeout_ms").parse().unwrap();
-    let mut client = cb::inference_client::InferenceClient::connect(host).await?;
+    let b: usize = std::env::args().nth(2).expect("missing number batch_size").parse().unwrap();
+    let n: usize = std::env::args().nth(3).expect("missing number iterations").parse().unwrap();
+    let timeout: u64 = std::env::args().nth(4).expect("missing timeout_ms").parse().unwrap();
+    println!(
+        "Host:{} BatchSize:{} Iterations:{} Timeout:{}",
+        host.clone(), b, n, timeout
+    );
+    println!("========================================================================================================================================================");
 
-    let mut latencies = vec![0u128; n];
+    let mut client = cb::inference_client::InferenceClient::connect(host).await?;
+    
+    let mut lat = vec![0u128; n];
+    let mut model_lat = vec![0u128; n];
+    let mut prep_lat = vec![0u128; n];
     for i in 1..n {
-        let request = Request::new(cb::PredictRequest {
-            cat_feature1: "A".to_string(),
-            cat_feature2: "B".to_string(),
-            cat_feature3: "C".to_string(),
-            float_feature4: 0.5,
-            float_feature5: 0.33,
-        });
+        let request = cb::PredictRequest {
+            features: vec![cb::Features {
+                cat_feature1: "A".to_string(),
+                cat_feature2: "B".to_string(),
+                cat_feature3: "C".to_string(),
+                float_feature4: 0.5,
+                float_feature5: 0.33,
+            }; b] 
+        };
         let start = Instant::now();
-        let response = client.predict(request).await?;
-        latencies[i] = start.elapsed().as_nanos();
+        let response = client.predict(request).await?.into_inner();
+        lat[i] = start.elapsed().as_nanos();
+        model_lat[i] = response.model_latency as u128;
+        prep_lat[i] = response.preprocess_latency as u128;
 
         if i % REPORT == 0 {
-            log_stats(i, n, timeout, &latencies, i - REPORT, REPORT);
+            log_stats(">Prepr", i, n, timeout, &prep_lat, i - REPORT, REPORT);
+            log_stats(">Model", i, n, timeout, &model_lat, i - REPORT, REPORT);
+            log_stats("Client", i, n, timeout, &lat, i - REPORT, REPORT);
+            println!("--------------------------------------------------------------------------------------------------------------------------------------------------------");
         }
     }
 
-    println!("REPORT ==================================================================================================================================");
-    log_stats(n, n, timeout, &latencies, 0, n);
-    println!("=========================================================================================================================================");
+    println!("REPORT =================================================================================================================================================");
+    log_stats(">Prepr", n, n, timeout, &prep_lat, 0, n);
+    log_stats(">Model", n, n, timeout, &model_lat, 0, n);
+    log_stats("Client", n, n, timeout, &lat, 0, n);
+    println!("========================================================================================================================================================");
     Ok(())
 }
 
-fn log_stats(i: usize, n: usize, timeout: u64, latencies: &Vec<u128>, skip: usize, take: usize) {
-    let mean = Duration::from_nanos((latencies.iter().skip(skip).take(take).sum::<u128>() / (i - skip) as u128) as u64);
-    let max = Duration::from_nanos(*latencies.iter().skip(skip).take(take).max().unwrap() as u64);
+fn log_stats(title: &str, i: usize, n: usize, timeout: u64, latencies: &Vec<u128>, skip: usize, take: usize) {
+    let mean = latencies.iter().skip(skip).take(take).sum::<u128>() / (i - skip) as u128;
+    let max = *latencies.iter().skip(skip).take(take).max().unwrap();
     let count = latencies.iter().skip(skip).take(take).collect::<Vec<&u128>>().len();
     let timeouts = latencies.iter().skip(skip).take(take).filter(
         |x| Duration::from_nanos(**x as u64) > Duration::from_millis(timeout)
@@ -53,26 +71,26 @@ fn log_stats(i: usize, n: usize, timeout: u64, latencies: &Vec<u128>, skip: usiz
     let ps: Vec<String> = percentiles(
         vec![0.95, 0.99, 0.999], latencies, skip, take
     ).iter().map(
-        |(p, x)| format!("p{:.1}: {:?}", 100.0 * p, x)
+        |(p, x)| format!("p{:.1}={:.3}ms", 100.0 * p, *x as f64 * 1e-6)
     ).collect();
 
     println!(
-        "{} -> {} Mean: {:?} Max: {:?} Count: {} Timeouts: {} Success: {}% {}",
-        skip,
+        "{}: \t{}\tMean={:.3}ms\tMax={:.3}ms\tCount={}\tTimeouts={}\tSucc={:0>3.3}%\t{}",
+        title,
         i,
-        mean,
-        max,
+        mean as f64 * 1e-6,
+        max as f64 * 1e-6,
         count,
         timeouts,
         success_ratio,
-        ps.join(" ")
+        ps.join("\t")
     );
 }
 
-fn percentiles(ps: Vec<f64>, latencies: &Vec<u128>, skip: usize, take: usize) -> Vec<(f64, Duration)> {
+fn percentiles(ps: Vec<f64>, latencies: &Vec<u128>, skip: usize, take: usize) -> Vec<(f64, u128)> {
     let mut sorted: Vec<&u128> = latencies.iter().skip(skip).take(take).collect();
     sorted.sort();
     ps.iter().map( |p|
-        (*p, Duration::from_nanos(*sorted[(sorted.len() as f64 * p) as usize] as u64))
+        (*p, *sorted[(sorted.len() as f64 * p) as usize])
     ).collect()
 }
--- rust/grpc-server/src/predict.rs
+++ rust/grpc-server/src/predict.rs
@@ -1,4 +1,6 @@
-use crate::cb::{PredictRequest, PredictResponse};
+use std::time::Instant;
+
+use crate::cb::{PredictRequest, PredictResponse, Features, Prediction};
 
 thread_local! {
     pub static MODEL: catboost::Model = load_model();
@@ -9,13 +11,33 @@ fn load_model() -> catboost::Model {
     catboost::Model::load("model.cbm").unwrap()
 }
 
+pub fn preprocess(features: &Vec<Features>) -> (Vec<Vec<String>>, Vec<Vec<f32>>) {
+    let cat_features = features.iter().map(
+        |f| vec![f.cat_feature1.clone(), f.cat_feature2.clone(), f.cat_feature3.clone()]
+    ).collect();
+
+    let float_features = features.iter().map(
+        |f| vec![f.float_feature4, f.float_feature5]
+    ).collect();
+
+    (cat_features, float_features)
+}
+
 pub fn predict(request: PredictRequest) -> PredictResponse {
-    PredictResponse { 
-        score: MODEL.with( |model|
-            model.calc_model_prediction(
-                vec![vec![request.float_feature4, request.float_feature5]],
-                vec![vec![request.cat_feature1, request.cat_feature2, request.cat_feature3]]
-            ).unwrap()[0] as f32
-        )
+    
+    let start = Instant::now();
+    let (cat_features, float_features ) = preprocess(&request.features);
+    let preprocess_latency = start.elapsed().as_nanos() as u64;
+
+    let start = Instant::now();
+    let pred = MODEL.with( |model| 
+            model.calc_model_prediction(float_features, cat_features).unwrap()
+    );
+    let model_latency = start.elapsed().as_nanos() as u64;
+    
+    PredictResponse {
+        predictions: pred.iter().map(|score| Prediction { score:  *score as f32 }).collect(),
+        preprocess_latency,
+        model_latency,
     }
 }
